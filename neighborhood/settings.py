36a2461bcfa4b6710c2c4ec8e72c6e83e0537e62
Bernice Twili <bernicetwili0@gmail.com>
2021-06-09 02:17:43 +0300

    trial

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -165,6 +165,8 @@ LOGOUT_REDIRECT_URL = 'index'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
