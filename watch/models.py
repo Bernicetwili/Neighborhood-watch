4cd6727ed441bf809778a0510339f54c36de8aa1
Bernice Twili <bernicetwili0@gmail.com>
2021-06-08 14:23:43 +0300

    add self argument

--- watch/models.py
+++ watch/models.py
@@ -40,12 +40,12 @@ class Profile(models.Model):
         return f'{self.user.username} profile'
 
     @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
+    def create_user_profile(self,sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
 
     @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
+    def save_user_profile(self,sender, instance, **kwargs):
         instance.profile.save()
 
 
